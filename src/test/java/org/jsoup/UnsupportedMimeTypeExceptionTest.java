6f674ddc2cf2f43139646dd5823b0fa162a054d8
Jad Bikarbass <jadbikarbass@dhcp-10-49-227-116.sf.umontreal.ca>
2024-09-30 16:12:00 -0400

    Test 1

--- src/test/java/org/jsoup/UnsupportedMimeTypeExceptionTest.java
+++ src/test/java/org/jsoup/UnsupportedMimeTypeExceptionTest.java
@@ -1,6 +1,8 @@
 package org.jsoup;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UnsupportedMimeTypeExceptionTest {
@@ -11,5 +13,17 @@ public class UnsupportedMimeTypeExceptionTest {
         assertTrue(exception.toString().contains("Unsupported MIME type"));
         assertTrue(exception.toString().contains("application/unsupported"));
         assertTrue(exception.toString().contains("http://example.com"));
+    }
+
+    @Test
+    public void testGetMimeType() {
+        UnsupportedMimeTypeException exception = new UnsupportedMimeTypeException("Unsupported MIME type", "application/unsupported", "http://example.com");
+        assertEquals("application/unsupported", exception.getMimeType());
+    }
+
+    @Test
+    public void testGetUrl() {
+        UnsupportedMimeTypeException exception = new UnsupportedMimeTypeException("Unsupported MIME type", "application/unsupported", "http://example.com");
+        assertEquals("http://example.com", exception.getUrl());
     }
 }
